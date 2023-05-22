0cc80a8590fa40612d325abb7272736e60413223
Eobago <125463075+Eobago@users.noreply.github.com>
2023-05-22 01:02:12 +0100

    Create exec_line.c

---
+++ exec_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int exec_line(data_shell *datash)
+{
+    int (*builtin)(data_shell *datash);
+
+    if (datash->args[0] == NULL)
+        return (1);
+
+    builtin = get_builtin(datash->args[0]);
+
+    if (builtin != NULL)
+        return (builtin(datash));
+
+    return (cmd_exec(datash));
+}
